8c71251f53f61add8caa530ab6020f453167b163
Thiago Assi <thiago-assi@outlook.com>
2024-04-13 13:36:24 -0300

    config: versioning with bump2version

--- tasks.py
+++ tasks.py
@@ -12,6 +12,17 @@ def lint(c):
 
 
 @task
+def bumpversion(c, part):
+    """
+    Bump the version number of the project.
+
+    The `part` parameter should be either
+    'major', 'minor' or 'patch'
+    """
+    c.run(f"bump2version {part}")
+
+
+@task
 def test(c):
     c.run("pytest")
 
