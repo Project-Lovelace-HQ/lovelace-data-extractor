3fe9bef952b85db4aaeca46bcd192fb76a5cbf84
Thiago Assi <thiago-assi@outlook.com>
2024-04-14 09:20:38 -0300

    refactor: separate class into file

--- src/ludopedia_website/fetch_boardgame_by_url.py
+++ src/ludopedia_website/fetch_boardgame_by_url.py
@@ -1,23 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-
-class SubscribedGameUpdatedData:
-    def __init__(self, city, condition, details, price, link):
-        self.city = city
-        self.condition = condition
-        self.details = details
-        self.price = price
-        self.link = link
-
-    def to_dict(self):
-        return {
-            "city": self.city,
-            "condition": self.condition,
-            "details": self.details,
-            "price": self.price,
-            "link": self.link,
-        }
+from src.util.subscribed_game_updated_data import SubscribedGameUpdatedData
 
 
 def fetch_boardgame_by_url(url):
